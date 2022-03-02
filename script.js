48cce46015127a980df14d8b33a8a89bea340900
Thomas Pendleton <thomas6576@yahoo.com>
2022-03-01 21:44:20 -0800

    adding css and axios api Get

---
+++ script.js
@@ -0,0 +1,29 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username)
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+// promise syntax
+// function getUser(username) {
+//     axios(APIURL + username)
+//     .then(response => console.log(response.data))
+//     .catch((error) => console.log(error))
+// }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const user = search.value
+
+  if(user){
+      getUser(user)
+      
+  }
+})
