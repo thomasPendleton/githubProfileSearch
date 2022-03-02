57c068ff1f4879f405cc1ada3eb2e575fca22faf
Thomas Pendleton <thomas6576@yahoo.com>
2022-03-02 10:35:14 -0800

    finished project

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const APIURL = 'https://api.github.com/users/'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const mainEl = document.getElementById('main')
 
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username)
-    console.log(data)
+    createUserCard(data)
+    getRepos(username)
   } catch (error) {
-    console.log(error)
+    if (error.response.status == 404) {
+      createErrorCard(`User "${username}" not found.`)
+    }
   }
 }
 // promise syntax
@@ -18,12 +22,62 @@ async function getUser(username) {
 //     .catch((error) => console.log(error))
 // }
 
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
+    addReposToCard(data)
+  } catch (error) {
+    createErrorCard('Problem fetching repos')
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = ` <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="" class="avatar">  
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers}<strong>Followers</strong></li>
+        <li>${user.following}<strong>Following</strong></li>
+        <li>${user.public_repos}<strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>`
+  mainEl.innerHTML = cardHTML
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+            <h1>${msg}</h1>
+    <div>
+    `
+  mainEl.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos')
+  repos.slice(0, 10).forEach((repo) => {
+    const repoEl = document.createElement('a')
+    repoEl.classList.add('repo')
+    repoEl.href = repo.html_url
+    repoEl.target = '_blank'
+    repoEl.innerText = repo.name
+    reposEl.appendChild(repoEl)
+  })
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const user = search.value
 
-  if(user){
-      getUser(user)
-      
+  if (user) {
+    getUser(user)
+    search.value = ''
   }
 })
